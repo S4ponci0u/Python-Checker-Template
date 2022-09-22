903d06d07b39db6097349641b24778cf8f402e15
Saponciou <79189550+S4ponci0u@users.noreply.github.com>
2022-09-22 23:30:31 +0200

    First release
    v0.1

---
+++ GradientGen/__init__.py
@@ -0,0 +1,72 @@
+# Made by Sir ChanChan | https://github.com/chanchan69/ConsoleGradient/blob/main
+from colour import Color
+from GradientGen.utils import linear_gradient
+
+
+class PrintGradient(object):
+    def __init__(self, start: str, end: str, message: str) -> None:
+        self.message = message
+        gradient = self.generate_gradient(start, end)
+        print(gradient)
+
+    def generate_gradient(self, start: str, end: str) -> str:
+        gradient = ""
+        start = Color(start)
+        colors = list(start.range_to(Color(end), count_real_characters(self.message)))
+
+        for m in list(self.message):
+            if m == "\n":
+                gradient += "\n"
+            elif m == "\r":
+                gradient += "\r"
+            elif m == " ":
+                gradient += " "
+            else:
+                c = colors[0]
+                r = int(c.rgb[0] * 256)
+                g = int(c.rgb[1] * 256)
+                b = int(c.rgb[2] * 256)
+                gradient += f"\033[38;2;{r};{g};{b}m{m}"
+                colors.remove(c)
+
+        return gradient
+
+
+class PrintLinearGradient(object):
+    def __init__(self, start: str, end: str, message: str) -> None:
+        self.message = message
+        gradient = self.generate_gradient(start, end)
+        print(gradient)
+
+    def generate_gradient(self, start: str, end: str) -> str:
+        gradient = ""
+        colors = linear_gradient(start, end, count_real_characters(self.message))
+        c = 0
+
+        for m in list(self.message):
+            if m == "\n":
+                gradient += "\n"
+            elif m == "\r":
+                gradient += "\r"
+            elif m == " ":
+                gradient += " "
+            else:
+                r = int(colors["r"][c])
+                g = int(colors["g"][c])
+                b = int(colors["b"][c])
+                gradient += f"\033[38;2;{r};{g};{b}m{m}"
+                c += 1
+
+        return gradient
+
+
+def count_real_characters(message: str) -> int:
+    count = 0
+
+    split = list(message)
+
+    for c in split:
+        if c != " " and c != "\n" and c != "\r":
+            count += 1
+
+    return count
---
+++ index.py
@@ -0,0 +1,72 @@
+import json
+from os import system
+import easygui
+import ctypes
+from truecolor import fg, reset
+import time
+from GradientGen import PrintGradient, PrintLinearGradient
+
+# Program variables
+checker_title = "Grubhub Checker"
+author = "S4ponci0u"
+
+# Accounts stats
+total_bad = 0
+total_checked = 0
+total_hits = 0
+
+def main():
+    # feel free to customize the title, if you dont know how to make ascii art, just go to 
+    # http://www.patorjk.com/software/taag/
+    title = '''
+  ▄████████    ▄████████    ▄███████▄ 
+  ███    ███   ███    ███   ███    ███ 
+  ███    █▀    ███    ███   ███    ███ 
+  ███          ███    ███   ███    ███    
+▀███████████ ▀███████████ ▀█████████▀        
+         ███   ███    ███   ███
+   ▄█    ███   ███    ███   ███ 
+ ▄████████▀    ███    █▀   ▄████▀                                                          
+    '''
+    system('cls')
+    # change title of the windows
+    ctypes.windll.kernel32.SetConsoleTitleW(f"{checker_title} | By: {author} | Checked: 0 | Hits: 0 | Bad: 0")
+    PrintLinearGradient("#C33764", "#1D2671", title)
+    print(f"{fg('#1C08FF')}\t[+] {fg('#87B5F5')}By: {author}{reset}")
+    print(f"{fg('#1C08FF')}\t[+] {fg('#87B5F5')}Select the combo{reset}")
+    try:
+        combo = easygui.fileopenbox()
+    except:
+        print('\t[!] No combo selected')
+        exit()
+
+    # you can also use proxys, just copy paste the code below and change the variable name
+    print(f"{fg('#1C08FF')}\t[+] {fg('#87B5F5')}Checking started!{reset}")
+    with open(combo, 'r') as f:
+        for line in f:
+            email, password = line.split(':')
+            check(email, password)
+
+def update_title(checked: bool = None, hit: bool = None, bad:bool = None):
+    if (checked):
+        global total_checked
+        total_checked += 1
+    if (hit):
+        global total_hits
+        total_hits += 1
+    if (bad):
+        global total_bad
+        total_bad += 1
+    ctypes.windll.kernel32.SetConsoleTitleW(f"{checker_title} | By: {author} | Checked: {total_checked} | Hits: {total_hits} | Bad: {total_bad}")
+
+
+def check(email:str, password:str):
+    # define your code here!
+    hit = True
+
+    if (hit):
+        print(f"{fg('green')}\t[+] {fg('white')}Hit: {email}:{password}{reset}")
+        update_title(checked=True, hit=True)
+    elif (hit == False):
+        update_title(checked=True, bad=True)
+main()
